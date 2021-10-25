517f9a4dc640c8bafef46ce8b56c5edd2d71fd34
Mohamadreza <mohammadreza.pishdad@lendo.ir>
2021-10-25 08:54:47 +0330

    Transaction service got refactored

--- src/TransactionService.php
+++ src/TransactionService.php
@@ -5,6 +5,7 @@ namespace Waxwink\Accounting;
 use Waxwink\Accounting\Concerns\HasPayload;
 use Waxwink\Accounting\Contracts\TransactionRepositoryInterface;
 use Waxwink\Accounting\Exceptions\InvalidPayloadException;
+use Waxwink\Accounting\Exceptions\LockedAccountException;
 use Waxwink\Accounting\Exceptions\TransactionFailedException;
 use Waxwink\Accounting\UseCases\InsertRecords;
 
@@ -26,49 +27,54 @@ class TransactionService
     }
 
     /**
-     * @throws TransactionFailedException
-     * @throws InvalidPayloadException
+     * @throws LockedAccountException
      */
     public function transfer(): mixed
     {
-        try {
-            return $this->lockHandler->handle(
-                fn() => $this->transactionalInsert() ,
-                $this->lockedAccountId,
-                $this->withLock);
-        } catch (InvalidPayloadException $exception) {
-            throw $exception;
-        } catch (\Exception $exception) {
-            throw new TransactionFailedException($exception->getMessage());
-        }
+        return $this->lockHandler->handle(
+            fn() => $this->transactionalInsert(),
+            $this->lockedAccountId,
+            $this->withLock);
     }
 
+    /**
+     * @throws LockedAccountException
+     */
     public function balance(int $accountId, mixed $ref = null): int
     {
-        return $this->transactionRepository->balance($accountId, $ref);
+        return $this->lockHandler->handle(
+            fn() => $this->transactionRepository->balance($accountId, $ref),
+            $this->lockedAccountId,
+            $this->withLock);
     }
-    
+
     public function transactionsList(int $accountId, array $options): \ArrayAccess
     {
         return $this->transactionRepository->findByAccount($accountId, $options);
     }
 
-    public function transactionsListByRef(mixed $refId, array $options): \ArrayAccess
-    {
-        return $this->transactionRepository->findByRef($refId, $options);
-    }
     public function withLock(int $accountId, bool $withLock = true): static
     {
-        $this->withLock = $withLock;
+        $this->withLock        = $withLock;
         $this->lockedAccountId = $accountId;
 
         return $this;
     }
 
+    /**
+     * @throws TransactionFailedException
+     * @throws InvalidPayloadException
+     */
     protected function transactionalInsert(): mixed
     {
-        return $this->transactionRepository->transactional(
-            fn() => $this->insertRecords->setTransactionPayload($this->transactionPayload)->fire());
+        try {
+            return $this->transactionRepository->transactional(
+                fn() => $this->insertRecords->setTransactionPayload($this->transactionPayload)->fire());
+        } catch (InvalidPayloadException $exception) {
+            throw $exception;
+        } catch (\Exception $exception) {
+            throw new TransactionFailedException($exception->getMessage());
+        }
     }
 
 
